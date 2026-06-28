1f382170e57ceaab7a2591ee857e35af5c7d0cfa
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-06-28 18:09:08 +0000

    Solved Problem FInd First Index of Target Using Recursion

--- Recursion/Recursion_On_Array/FindFirstIndex.java
+++ Recursion/Recursion_On_Array/FindFirstIndex.java
@@ -1,25 +1,21 @@
 public class FindFirstIndex{
 
-    // Approach 1
     public static int findFirstOccu(int arr[],int idx,int key){
+        // base case
         if(idx >= arr.length) return -1;
 
-        return arr[idx] == key ? idx : findFirstOccu(arr,idx+1,key);
-    }
+        // Faith --> Function will found the first index of key from idx+1 To (n-1) elemets
+        int soFarIdx = findFirstOccu(arr,idx+1,key);
 
-    // Approach 2
+        // My JOb --> Check that is current value is the first index of key
+        if(arr[idx] == key) return idx;
 
-     public static int findFirstOccu2(int arr[],int idx,int key){
-        if(idx >= arr.length) return -1;
-
-        int findIdxForward = findFirstOccu2(arr,idx+1,key);
-
-        return arr[idx] == key ? idx : findIdxForward;
+        return soFarIdx;
     }
     public static void main(String args[]){
-        int arr[] = {9,10,2,3,2,2,4,5};
+        int arr[] = {9,10,2,3,2,2,4,5,9};
 
-        System.out.println(findFirstOccu(arr,0,7));
-        System.out.println(findFirstOccu2(arr,0,7));
+        System.out.println(findFirstOccu(arr,0,9));
+
     }
 }
