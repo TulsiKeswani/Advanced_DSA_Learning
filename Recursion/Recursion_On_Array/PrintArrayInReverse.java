4a40df7e84c669082bef259707d06f947ac41502
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-06-28 14:53:18 +0000

    Solved Problem Print Array In Reverse Using two base Cases and Recursion

--- Recursion/Recursion_On_Array/PrintArrayInReverse.java
+++ Recursion/Recursion_On_Array/PrintArrayInReverse.java
@@ -1,13 +1,22 @@
 public class PrintArrayInReverse{
     public static void printArrRev(int arr[],int idx){
+        // base case 
         if(idx >= arr.length) return;
+        if(idx < 0) { 
+            printArrRev(arr,0);
+            return;
+        }
 
+        // faith --> Function will print (n-1) elements in reverse order
         printArrRev(arr,idx+1);
+
+        // My Job
         System.out.print(arr[idx] + " ");
+
     }
     public static void main(String args[]){
         int arr[] = {9,4,17,20,2};
 
-        printArrRev(arr,0);
+        printArrRev(arr,-10);
     }
 }
