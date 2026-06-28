7d78407d9fc9cd17806e5cb4b61927d242e72c5c
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-06-28 16:32:15 +0000

    Solved Problem Find Maximum of Array using recursion

--- Recursion/Recursion_On_Array/FindMaxInArray.java
+++ Recursion/Recursion_On_Array/FindMaxInArray.java
@@ -1,11 +1,17 @@
 public class FindMaxInArray{
 
     public static int findMax(int arr[],int idx){
-        if(idx >= arr.length) return Integer.MIN_VALUE;
 
-        int forwardMax = findMax(arr,idx+1);
+        // Base Case 
+        if(idx >= arr.length){
+            return Integer.MIN_VALUE;
+        }
 
-        return Math.max(forwardMax,arr[idx]);
+        // Faith --> Function will return maximum from (n-1) elements 
+        int maxSoFar = findMax(arr,idx+1);
+
+        // My JOb --> Compare current element with soFar max
+        return Math.max(maxSoFar,arr[idx]);
     }
     public static void main(String args[]){
         int arr[] = {5,14,9,6,7};
---
+++ Recursion/Recursion_On_Array/StairsPath.java
@@ -0,0 +1,5 @@
+public class StairsPath{
+    public static void main(String args[]){
+        
+    }
+}
