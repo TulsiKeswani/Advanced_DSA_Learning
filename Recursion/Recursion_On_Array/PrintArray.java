db2031b820b631c0ac7393fea6517fd39a6821da
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-06-28 14:44:19 +0000

    Solved Problem Print Array Elements using Recursion

--- Recursion/Recursion_On_Array/PrintArray.java
+++ Recursion/Recursion_On_Array/PrintArray.java
@@ -1,13 +1,15 @@
 public class PrintArray{
+   
     public static void printArr(int arr[],int idx){
         if(idx >= arr.length) return;
-
         System.out.print(arr[idx] + " ");
         printArr(arr,idx+1);
     }
+
     public static void main(String args[]){
         int arr[] = {9,5,17,20,4};
 
         printArr(arr,0);
+        System.out.println();
     }
 }
